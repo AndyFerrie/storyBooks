e9640c6fe3116c566e0de2a8e1e71db99b5a86ed
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-02-24 00:44:56 +0000

    Add passport.js 48:36

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db.js')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -29,6 +34,18 @@ app.engine(
     })
   )
   app.set('view engine', '.hbs')
+
+  // Sessions
+
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }))
+
+  // Passport middleware
+  app.use(passport.initialise())
+  app.use(passport.session())
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
